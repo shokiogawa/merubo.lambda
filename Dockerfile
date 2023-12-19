f8b13798b6df9a3bfc18abc64432d6386bbaf4db
shokiogawa <shouki2578@gmail.com>
2023-12-19 15:25:19 +0900

    add

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:1.21.5-alpine as builder
+WORKDIR /go/merubo/
+COPY ../src ./src
+COPY ../go.mod go.sum ./
+RUN apk update && apk --no-cache add git
+RUN go mod tidy
+WORKDIR /go/merubo/src
+RUN CGO_ENABLE=0 GOOS=linux go build -o /go/merubo/binary
+
+
+#production
+FROM alpine as production
+WORKDIR go/merubo/production
+RUN apk add --no-cache ca-certificates && apk add curl
+COPY --from=builder /go/merubo/binary /go/merubo/production
+ENV PORT=${PORT}
+CMD ["/go/merubo/production/binary"]
---
+++ src/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"log/slog"
+)
+
+// 参考
+// https://yhidetoshi.hatenablog.com/entry/2022/10/10/145121
+
+//var echoLambda *echoadapter.EchoLambda
+
+func init() {
+	slog.Info("コールドスタート")
+	//e := echo.New()
+	//e.Use(middleware.Recover())
+	//e.GET("/api/contact", GetContact)
+	//e.POST("/api/contact", CreateContact)
+	//
+	//// セッティング
+	//echoLambda = echoadapter.New(e)
+}
+
+func main() {
+	slog.Info("Goアプリ開始")
+	lambda.Start(HandleRequest)
+}
+
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	slog.Info("HandlerRequest")
+	return events.APIGatewayProxyResponse{
+		Body:       "Hello",
+		Headers:    nil,
+		StatusCode: 200}, nil
+}
